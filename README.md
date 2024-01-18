f44f4767461fed28df5966f95b74c73c2c577876
Park JongBeum <dev@parkjb.com>
2024-01-18 19:35:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Keyword Similarity
+
+This Project depends on local LLM interface Ollama.
+
+## How to run
+Install [Ollama](https://ollama.ai) and open server via `ollama serve`
+You may need to pull models via `ollama pull`
+
+Run `pipenv install` to install all dependencies
+`pipenv run python scrap.py` to run the project or `pipenv shell` to enter the virtual environment and run `python scrap.py`
+
+```bash
+pipenv install # install dependencies
+pipenv shell # enter virtual environment
+python scrap.py # run the project
+```
+
+
---
+++ scrap.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests as req
+import json
+from ast import literal_eval
+import os
+
+
+def get_html(url):
+    res = req.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+
+def get_article_urls():
+    doc = get_html("https://hipandpelvis.or.kr/index.php?body=archive")
+    anchors = doc.select(".accordion-collapse a")
+
+    urls = []
+
+    for anchor in anchors:
+        url = f'https://hipandpelvis.or.kr/{anchor["href"]}'
+        doc = get_html(url)
+        anchors = doc.select(".ToC_title a")
+        for anchor in anchors:
+            article_url = f'https://hipandpelvis.or.kr/{anchor["href"]}'
+            print(article_url)
+            urls.append(article_url)
+
+    return urls
+
+
+def get_abstract(url):
+    doc = get_html(url)
+    title = doc.select("h1.content-title")[0].text.strip()
+    doiurl = doc.select(".article-meta-doi-link")[0].text.strip()
+    divs = doc.select(".article-abstract h3, .article-abstract p")
+    content = ""
+
+    for div in divs:
+        content += f"{div.get_text()}\n"
+
+    return {"title": title, "url": doiurl, "content": content}
+
+
+def chatbot(prompt):
+    res = req.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "mistral",
+            # 'model': 'llama2-uncensored',
+            "prompt": prompt,
+            "stream": False,
+        },
+    ).text
+    ans = json.loads(res)["response"]
+    return ans
+
+
+if __name__ == "__main__":
+    urls = []
+
+    if (os.path.exists("urls.txt")):
+        with open("urls.txt", "r") as f:
+            urls = f.read().split("\n")
+    else:
+        urls = get_article_urls()
+        with open("urls.txt", "w") as f:
+            for url in urls:
+                f.write(url + "\n")
+
+    articles = []
+
+    prompt = "Return the keyword of the following abstract. "
+    prompt_form = "The answer MUST BE ONLY a python array (such as ['keyword1', 'keyword2', 'keyword3']) since the results will be directly delivered to python code. "
+
+    for url in urls:
+        article = get_abstract(url)
+
+        while True:
+            try:
+                article['keywords'] = literal_eval(chatbot(prompt + prompt_form + article['content']))
+            except Exception as e:
+                print(e)
+                continue
+            break
+
+        print(article)
+        articles.append(article)
+
+        with open("articles.json", "w") as f:
+            json.dump(articles, f)
+
